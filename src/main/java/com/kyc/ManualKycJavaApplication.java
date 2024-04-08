def51c94e4ed382d32d4ebc3f8850d2aa30e73fe
stlaphome <stlaphome@gmail.com>
2024-04-08 10:34:08 +0530

    Initial Commit

---
+++ src/main/java/com/kyc/ManualKycJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualKycJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualKycJavaApplication.class, args);
+	}
+
+}
