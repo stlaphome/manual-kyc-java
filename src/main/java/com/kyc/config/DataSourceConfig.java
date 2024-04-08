c393d304d6477704e24b02dc36c39ded0e378802
stlaphome <stlaphome@gmail.com>
2024-04-08 10:48:39 +0530

    refs manual kyc initial commit

---
+++ src/main/java/com/kyc/config/DataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.kyc.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource(value = {"file:${STLAP_LMS}/lms_application.properties"})
+public class DataSourceConfig {
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverName;
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Value("${spring.datasource.username}")
+	private String username;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@Bean
+	public DataSource getDataSource() throws Exception {
+		
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(driverName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+		
+	}
+	
+}
